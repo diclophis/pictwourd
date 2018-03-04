59856478177c6eec5e5cf665c57236e228e956d9
Jon Bardin <diclophis@gmail.com>
2018-03-04 06:43:39 +0000

    add first whack at statis react site

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', "@babel/es2015", "@babel/react"]
+          }
+        }
+      }
+    ],
+  },
+
+  entry: './static.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  }
+};
+
