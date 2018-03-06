c0dc783a459c55e97df70ac1e083874c6c3c1e42
Jon Bardin <diclophis@gmail.com>
2018-03-06 10:56:37 +0000

    this makes firefox warning

--- static.js
+++ static.js
@@ -15,17 +15,12 @@ class App extends React.Component {
     await this.onFoo();
   }
 
-  async onFoo() {
+  async onFoo(newImage = null) {
     let manifestIndexJson = await import(
       './build/index.manifest/manifest.json'
     );
 
-
-    //let manifestIndexJson = await import(
-    //  './build/index.manifest/manifest.json'
-    //);
-
-    let randomInt = (parseInt(Math.random() * manifestIndexJson.length) + 1);
+    let randomInt = newImage ? newImage : (parseInt(Math.random() * manifestIndexJson.length) + 1);
     let jsonFileToLoad = './build/index.manifest/' + randomInt.toString() + '.json'
     let otherJson = await import(`./build/index.manifest/${randomInt}.json`);
 
@@ -33,35 +28,10 @@ class App extends React.Component {
       manifestIndexJson: manifestIndexJson,
       relatedImages: otherJson['results'],
       activeImage: randomInt
-    }); // + manifestIndexJson[0]["filename"] };
-
-    console.log(otherJson, randomInt);
-
-    ////import otherJson from './build/index.manifest/999.json';
-    ////let otherJson = await import(/* webpackChunkName: 'cheeze' */ `./build/index.manifest/${randomInt}.json`);
-
-    ////let fooContext = require.context("cheeze", false, regExp = /^\.json$/);
-    ////console.log(fooContext, fooContext.keys());
-
-    ////.import
-
-    ////let otherJson = await System.import(
-    ////  /* 
-    ////    webpackMode: "lazy", 
-    ////    webpackChunkName: "cheeze"
-    ////  */
-    ////  '999.json'
-    ////);
-
-    ////await import otherJson from jsonFileToLoad;
-
-    //console.log("cheeese", otherJson['results'][0]['indexNumber'], randomInt, otherJson);
-    //this.setState({ message: 'fooooo' + manifestIndexJson[0]["filename"]  + otherJson['results'][0]['indexNumber']});
-
+    });
   }
 
   render() {
-    //let { message } = this.state;
     let newUrl = null;
     let otherImages = null;
 
@@ -84,20 +54,16 @@ class App extends React.Component {
           }
 
           if (index == 0) {
-            //style = {height: "50%", margin: "0.5em 0.5em 0.5em 0.5em", clear: "none" };
             style = {order: 0, margin: "0.5em", flex: `0 1 ${vvv - index}em`, alignSelf: "auto"}
           } else {
-            //style = {height: "10%", margin: "0.5em", clear: "none", verticalAlign: "bottom" };
             style = {order: 0, margin: "0.5em", flex: `0 1 ${newHeight}em`, alignSelf: "center"};
           }
 
           return (
-            <img key={otherUrl} style={style} src={otherUrl}/>
+            <img key={otherUrl} style={style} src={otherUrl} onClick={this.onFooClick(otherImage.indexNumber)}/>
           )
         });
       }
-
-      console.log("WTF", newUrl);
     }
 
     let flexContainer = {
