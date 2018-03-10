7e0049e7b787239254c3803ca25d7974c88e3720
Jon Bardin <diclophis@gmail.com>
2018-03-10 08:58:01 +0000

    clean up css and make history back button work

--- static.js
+++ static.js
@@ -2,35 +2,74 @@ import React from 'react';
 
 //import manifestIndexJson from './build/index.manifest/manifest.json';
 
+var styles = cssInJS((context) => {
+  let vvv = 33;
+
+  let foop = {
+    flexContainer: {
+      display: "flex",
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: "flex-start",
+      alignContent: "flex-start",
+      alignItems: "flex-start"
+    }
+  };
+
+  for (let i = 0; i<vvv; i++) {
+    let newHeight = vvv - 15 - (i * 1.5);
+    if (newHeight < 5) {
+      newHeight = 5;
+    }
+
+    let primary = {
+        cursor: 'pointer',
+        transition: "none 0s",
+        order: 0,
+        margin: "0.5em", 
+        flex: `0 1 ${vvv}em`, alignSelf: "auto"
+    }
+
+    if (i !=0) {
+      primary['flex'] = `0 1 ${newHeight}em`;
+      primary['alignSelf'] = "center";
+    }
+
+    foop['image' + i] = primary;
+  }
+
+  return foop;
+});
+
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-  
-    //this.onFooClick = () => this.onFoo();
-
     this.state = {};
   }
 
   async componentDidMount() {
     await this.onFoo();
+    window.addEventListener("popstate", (ev) => {
+      this.setState(ev.state);
+    });
   }
 
   async onFoo(newImage) {
-    let manifestIndexJson = await import(
-      './build/index.manifest/manifest.json'
-    );
-
-    console.log(newImage);
-
+    let manifestIndexJson = await import('./build/index.manifest/manifest.json');
     let randomInt = newImage ? newImage : (parseInt(Math.random() * manifestIndexJson.length) + 1);
     let jsonFileToLoad = './build/index.manifest/' + randomInt.toString() + '.json'
     let otherJson = await import(`./build/index.manifest/${randomInt}.json`);
 
-    this.setState({
-      manifestIndexJson: manifestIndexJson,
-      relatedImages: otherJson['results'],
-      activeImage: randomInt
-    });
+		var stateObj = {
+			manifestIndexJson: manifestIndexJson,
+			relatedImages: otherJson['results'],
+			activeImage: randomInt
+		};
+
+		history.pushState(stateObj, "image" + randomInt, "?id=" + randomInt);
+
+		this.setState(stateObj);
   }
 
   render() {
@@ -44,58 +83,40 @@ class App extends React.Component {
       let relatedImages = this.state.relatedImages;
 
       if (relatedImages) {
-        let vvv = 33;
         otherImages = relatedImages.map((otherImage, index) => {
           let otherUrl = manifestIndexJson[otherImage.indexNumber]["filename"].replace("/home/ubuntu/pictwourd/build", "");
 
-          let style = {};
-
-          let newHeight = vvv - 15 - (index * 1.5);
-          if (newHeight < 5) {
-            newHeight = 5;
-          }
-
           let newIndex = null;
 
-          if (index == 0) {
-            style = {cursor: 'pointer', transition: "none 0s", order: 0, margin: "0.5em", flex: `0 1 ${vvv - index}em`, alignSelf: "auto"}
-          } else {
+          let style = styles['image' + index];
+
+          if (index != 0) {
             newIndex = otherImage.indexNumber;
-            style = {cursor: 'pointer', transition: "none 0s", order: 0, margin: "0.5em", flex: `0 1 ${newHeight}em`, alignSelf: "center"};
           }
 
           return (
-            <img key={otherUrl} style={style} src={otherUrl} onClick={this.onFoo.bind(this, newIndex)}/>
+            <img key={otherUrl} className={style} src={otherUrl} onClick={this.onFoo.bind(this, newIndex)}/>
           )
         });
       }
     }
 
-    let flexContainer = {
-      display: "flex",
-      flexDirection: "row",
-      flexWrap: "wrap",
-      justifyContent: "flex-start",
-      alignContent: "flex-start",
-      alignItems: "flex-start"
-    };
-
     return (
-      <div>
-        <div style={flexContainer}>
-          {otherImages}
-        </div>
+      <div className={styles.flexContainer}>
+        {otherImages}
       </div>
     );
   }
 }
 
+// pure functional is cooler?
 const Html = (props) => {
   return (
     <html>
       <head>
         <meta httpEquiv="Content-Type" content="text/html;charset=utf-8"/>
         <title>App</title>
+        <link rel="stylesheet" href="bundle.css"/>
       </head>
       <body>
         <div id="app">{props.children}</div>
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
