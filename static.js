8fdd5f1335a2288f15e598d1ecce49230ec86fa3
Jon Bardin <diclophis@gmail.com>
2018-03-10 10:48:07 +0000

    add fancy background and better css

--- static.js
+++ static.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import ImagePalette from 'react-image-palette';
 
 //import manifestIndexJson from './build/index.manifest/manifest.json';
 
 var styles = cssInJS((context) => {
-  let vvv = 33;
+  let vvv = 28;
 
   let foop = {
+		'$html, body': {
+			margin: 0,
+			padding: 0,
+		},
+
     flexContainer: {
       display: "flex",
       flexDirection: "row",
@@ -16,8 +22,8 @@ var styles = cssInJS((context) => {
     }
   };
 
-  for (let i = 0; i<vvv; i++) {
-    let newHeight = vvv - 15 - (i * 1.5);
+  for (let i = 0; i<32; i++) {
+    let newHeight = vvv - 17 - (i * 1.75);
     if (newHeight < 5) {
       newHeight = 5;
     }
@@ -27,12 +33,15 @@ var styles = cssInJS((context) => {
         transition: "none 0s",
         order: 0,
         margin: "0.5em", 
-        flex: `0 1 ${vvv}em`, alignSelf: "auto"
+        flex: `0 1 ${vvv}em`, alignSelf: "auto",
     }
 
     if (i !=0) {
       primary['flex'] = `0 1 ${newHeight}em`;
       primary['alignSelf'] = "center";
+    } else {
+      //primary['border'] = "0.333em solid white";
+      primary['margin-left'] = "14em";
     }
 
     foop['image' + i] = primary;
@@ -49,7 +58,7 @@ class App extends React.Component {
   }
 
   async componentDidMount() {
-    await this.onFoo();
+    await this.onFoo(this.props.initialImage);
     window.addEventListener("popstate", (ev) => {
       this.setState(ev.state);
     });
@@ -67,7 +76,7 @@ class App extends React.Component {
 			activeImage: randomInt
 		};
 
-		history.pushState(stateObj, "image" + randomInt, "?id=" + randomInt);
+		history.pushState(stateObj, "image" + randomInt, "?" + randomInt + "");
 
 		this.setState(stateObj);
   }
@@ -75,6 +84,8 @@ class App extends React.Component {
   render() {
     let newUrl = null;
     let otherImages = null;
+    let firstImage = null;
+    let fooop = null;
 
     if (this.state.manifestIndexJson && this.state.manifestIndexJson[this.state.activeImage]) {
       newUrl = this.state.manifestIndexJson[this.state.activeImage]["filename"].replace("/home/ubuntu/pictwourd/build", "");
@@ -82,30 +93,72 @@ class App extends React.Component {
       let manifestIndexJson = this.state.manifestIndexJson;
       let relatedImages = this.state.relatedImages;
 
+      let filterFunA = (otherImage, index) => {
+        let r = {};
+        r['otherUrl'] = manifestIndexJson[otherImage.indexNumber]["filename"].replace("/home/ubuntu/pictwourd/build", "");
+        r['style'] = styles['image' + index];
+        r['newIndex'] = otherImage.indexNumber;
+        return r;
+      };
+
       if (relatedImages) {
-        otherImages = relatedImages.map((otherImage, index) => {
-          let otherUrl = manifestIndexJson[otherImage.indexNumber]["filename"].replace("/home/ubuntu/pictwourd/build", "");
-
-          let newIndex = null;
-
-          let style = styles['image' + index];
-
-          if (index != 0) {
-            newIndex = otherImage.indexNumber;
-          }
-
-          return (
-            <img key={otherUrl} className={style} src={otherUrl} onClick={this.onFoo.bind(this, newIndex)}/>
-          )
+        firstImage = relatedImages.map(filterFunA).find((otherImage, index) => {
+          return (index == 0);
         });
+
+        fooop = (color, alternativeColor) => {
+          return (
+            relatedImages.map(filterFunA).map((otherImage, index) => {
+              let extraStyle = { };
+
+              let img = (
+                <img 
+                  key={otherImage['otherUrl']} 
+                  style={extraStyle} className={otherImage['style']}
+                  src={otherImage['otherUrl']}
+                  onClick={this.onFoo.bind(this, otherImage['newIndex'])}
+                />
+              );
+
+              let span = null
+
+              if (index == 0) {
+                //extraStyle['border'] = "0.1em solid " + color;
+                extraStyle['borderLeft'] = "0.33em solid " + alternativeColor;
+                extraStyle['paddingLeft'] = "1em";
+                span = (img);
+              } else {
+                span = (img);
+              }
+
+              return (span);
+            })
+          );
+        };
       }
     }
 
-    return (
-      <div className={styles.flexContainer}>
-        {otherImages}
-      </div>
-    );
+    if (firstImage) {
+      return (
+        <ImagePalette image={firstImage['otherUrl']} key={firstImage['otherUrl']}>
+          {({ backgroundColor, color, alternativeColor }) => (
+            <div style={{backgroundColor, color}}>
+              <p style={{margin: "1em", width: "12em", float: "left", position: "absolute"}}>
+                {firstImage['newIndex']}
+              </p>
+              <div className={styles.flexContainer}>
+                {fooop(color, alternativeColor)}
+              </div>
+            </div>
+          )}
+        </ImagePalette>
+      );
+    } else {
+      return (
+        <div className={styles.flexContainer}>
+        </div>
+      );
+    }
   }
 }
 
