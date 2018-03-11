0931efd4e09edac6f8fcbb4b01193d61d73256c1
Jon Bardin <diclophis@gmail.com>
2018-03-11 23:10:00 +0000

    fixes for randomization

--- browser.js
+++ browser.js
@@ -1,9 +1,10 @@
 //
+console.log("browser javascript starts here")
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App, Html } from './static';
 
-const initialData = { initialImage: window.location.search.replace("?", "") };
+const initialData = { initialImage: parseInt(window.location.search.replace("?", "")) || false };
 
 ReactDOM.hydrate(<App {...initialData} />, document.getElementById('app'));
